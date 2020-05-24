b797bb48633824a60ea44c941b9a220c7156f712
xkhaloda@gmail.com <xkhaloda@gmail.com>
2020-05-24 08:10:28 +0000

    he

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+c.PromptManager.in_template = r'>>> '
+c.PromptManager.out_template = r'[1] '
+c.InteractiveShell.colors = 'NoColor'
+c.HistoryManager.enabled = False
+c.InteractiveShellApp.extensions = [
+  'ipython_nose'
+]
